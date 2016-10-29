a712636fb109c1e4262779fc8dcbbb0a6099e312
KatherineYu <katherineyu@tests-Mac-Pro.local>
2016-10-29 06:55:57 -0700

    init metrics

---
+++ util/evaluationmetrics.py
@@ -0,0 +1,40 @@
+#python 2.7
+import numpy as np
+
+def apk(actual, predicted, k=7):
+    
+    if type(predicted) == str:
+        predicted = predicted.split()
+    
+    if len(predicted) > k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+def dcg_at_k(r, k, method=0):
+    r = np.asfarray(r)[:k]
+    if r.size:
+        if method == 0:
+            return r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
+        elif method == 1:
+            return np.sum(r / np.log2(np.arange(2, r.size + 2)))
+        else:
+            raise ValueError('method must be 0 or 1.')
+    return 0.
+
+def ndcg_at_k(r, k, method=0):
+    dcg_max = dcg_at_k(sorted(r, reverse=True), k, method)
+    if not dcg_max:
+        return 0.
+    return dcg_at_k(r, k, method) / dcg_max
